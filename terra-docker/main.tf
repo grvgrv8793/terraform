b25d8b78d053ba814eebe204adbeef541c8a5a70
Gaurav Lalwani <lalwani.gaurav81@gmail.com>
2022-01-13 02:51:28 +0530

    Terraform Basic Code

---
+++ terra-docker/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+    name = "nodered_app"
+    image = docker_image.nodered_image.latest  
+    ports{
+        internal = 1880
+        external = 1880
+    }
+}
+
+output "IP-Address" {
+  value = docker_container.nodered_container.ip_address
+  description = "IP address of container"
+}
+
+output "Container-name"{
+  value = docker_container.nodered_container.name
+  description = "the name of conatiner"
+}
