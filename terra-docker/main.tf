80fcda8a3bb85e73c4d3b0fc163442fbef4fb459
Gaurav Lalwani <lalwani.gaurav81@gmail.com>
2022-01-25 05:36:14 +0530

    Adding Variables and variables validation

--- terra-docker/main.tf
+++ terra-docker/main.tf
@@ -14,12 +14,33 @@ terraform {
 provider "docker" {}
 provider "random" {}
 
+variable "ext_port" {
+  type    = number
+  default = 1880
+
+  validation {
+    condition     = var.ext_port <= 65535 && var.ext_port > 0
+    error_message = "The external port should be in range 0 - 65535."
+  }
+}
+
+variable "int_port" {
+  type    = number
+  default = 1880
+}
+
+variable "container_count" {
+  type    = number
+  default = 2
+}
+
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random1" {
-  count   = 2
+  count   = var.container_count
   length  = 4
   special = false
   upper   = false
@@ -27,12 +48,12 @@ resource "random_string" "random1" {
 
 
 resource "docker_container" "nodered_container" {
-  count = 2
+  count = var.container_count
   name  = join("-", ["nodered_app", random_string.random1[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
-    internal = 1880
-    #external = 1880
+    internal = var.int_port
+    external = var.ext_port
   }
 }
 
@@ -43,7 +64,7 @@ output "Container-name" {
 }
 
 output "IP-Address" { // This is how we use for loop, here we are ittrating over container and fetching values
-  value       = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address],i.ports[*]["external"])]
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
   description = "IP address of container"
 }
 
